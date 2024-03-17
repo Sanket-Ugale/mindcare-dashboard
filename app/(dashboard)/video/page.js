f11853f3bead67492e914852e2c1cd24bf9afa12
Sanket-Ugale <sanketugale683@outlook.com>
2024-03-17 13:47:03 +0530

    Remove unused environment variables and update video call page

--- app/(dashboard)/video/page.js
+++ app/(dashboard)/video/page.js
@@ -1,17 +1,22 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const VideoCallPage = () => {
+  const [isWindowDefined, setIsWindowDefined] = useState(false);
   const [videoCall, setVideoCall] = useState(false);
+
+  useEffect(() => {
+    setIsWindowDefined(typeof window !== "undefined");
+  }, []);
 
   const callbacks = {
     EndCall: () => setVideoCall(false),
   };
 
-
-  const AgoraUIKit =
-    typeof window !== "undefined" && require("agora-react-uikit");
+  const AgoraUIKit = dynamic(() => import("agora-react-uikit"), {
+    ssr: false,
+  });
 
   const rtcProps = {
     appId: "ced4e343c11f4cefb701f5c8ec2bf880",
@@ -20,18 +25,22 @@ const VideoCallPage = () => {
       "007eJxTYIi/LC9y2cVY6Nd94af2F2TNf0jrMX7prRCSSb2lfzTgt7cCQ1KqaVqyRWpyqnGapYmhuamFhVFSsqGZYZpBSpKhYUryrYXfUhsCGRn0tV8yMEIhiM/D4FhQkJ+ZV5KbmldiyMAAABGAIkY=",
   };
 
-  return videoCall ? (
-    <div style={{ display: "flex", height: "100vh" }}>
-      {AgoraUIKit && <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />}
-    </div>
-  ) : (
-    <button
-      className="px-2 py-1 bg-blue-500 text-white rounded-md"
-      onClick={() => setVideoCall(true)}
-    >
-      Join
-    </button>
-  );
+  return isWindowDefined ? (
+    videoCall ? (
+      <div
+        style={{ display: "flex", justifyContent: "center", height: "90vh" }}
+      >
+        <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+      </div>
+    ) : (
+      <button
+        className="px-2 py-1 bg-blue-500 text-white rounded-md"
+        onClick={() => setVideoCall(true)}
+      >
+        Join
+      </button>
+    )
+  ) : null;
 };
 
 export default VideoCallPage;
